20e69fdd638d318fb3f99248dee4577d545f7953
Valentyn Digtiarenko <baggins999@gmail.com>
2022-09-24 18:24:53 +0300

    Finished styles

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,46 +1,39 @@
 import './message.scss';
+import { MESSAGE_TYPE } from '../../constants/messageType';
 
 export const Message = ({ type }) => {
    let styles;
-   console.log(type);
-
    switch (type) {
       case 'income':
-         styles = {
-            alignSelf: 'flex-start',
-            borderRadius: '0px 15px 15px 0px',
-         };
+         styles = MESSAGE_TYPE.income;
          break;
       case 'outcome':
-         styles = {
-            alignSelf: 'flex-end',
-            borderRadius: '15px 0px 0px 15px',
-            flexDirection: 'row-reverse',
-         };
+         styles = MESSAGE_TYPE.outcome;
          break;
       default:
          break;
    }
-   console.log(styles);
 
    return (
       <div className="message" style={styles}>
          <div className="messageInfo">
             <img
+               className="avatar"
                src="https://static.wixstatic.com/media/9fd359_d63e59f338694736affc6fa986369c30~mv2.png/v1/fill/w_315,h_409,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Facepic.png"
                alt="avatar"
             />
             <span>just now</span>
          </div>
          <div className="messageContent">
-            <p>
+            {/* <p>
                message Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
                consectetur adipisicing elit. Quaerat, sed?
-            </p>
-            {/* <img
+            </p> */}
+            <img
+               className="userImg"
                src="https://static.wixstatic.com/media/9fd359_d63e59f338694736affc6fa986369c30~mv2.png/v1/fill/w_315,h_409,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Facepic.png"
                alt="avatar"
-            /> */}
+            />
          </div>
       </div>
    );
---
+++ src/constants/messageType.js
@@ -0,0 +1,11 @@
+export const MESSAGE_TYPE = {
+   outcome: {
+      alignSelf: 'flex-end',
+      borderRadius: '15px 0px 0px 15px',
+      flexDirection: 'row-reverse',
+   },
+   income: {
+      alignSelf: 'flex-start',
+      borderRadius: '0px 15px 15px 0px',
+   },
+};
